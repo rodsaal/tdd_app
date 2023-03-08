77352f2a84763325272d60714884579bfd17bded
rodsaal <rodrigo.rsaal@gmail.com>
2023-03-07 22:16:26 -0300

    feat(customer): Creating edit page and refactoring controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update]
   def index
     @customers = Customer.all
   end
@@ -18,6 +19,22 @@ class CustomersController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id), notice: 'Cliente atualizado com sucesso!'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_customer
     @customer = Customer.find(params[:id])
   end
 

1259bdd050205e97ab82b70dd89d7fc68d2d04e3
rodsaal <rodrigo.rsaal@gmail.com>
2023-03-07 21:23:26 -0300

    feat(customer): Creating customer show page

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -38,4 +38,20 @@ RSpec.feature "Customers", type: :feature do
     click_on('Criar Cliente')
     expect(page).to have_content('não pode ficar em branco')
   end
+
+  scenario 'Mostra um cliente' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S', 'N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customer_path(customer.id))
+    expect(page).to have_content(customer.name)
+    expect(page).to have_content(customer.email)
+    expect(page).to have_content(customer.phone)
+    expect(page).to have_content(customer.smoker)
+  end
 end
